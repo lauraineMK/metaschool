606b57b4544299943d003001e85db39beecaa73a
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-08-21 17:05:10 +0200

    Controllers of the main entities involved in the course made

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\ModuleController;
 use App\Http\Controllers\SectionController;
 
 
@@ -13,6 +15,10 @@ Route::get('courses', [CourseController::class, 'index']);
 Route::get('courses/{id}', [CourseController::class, 'show']);
 Route::get('sections', [SectionController::class, 'index']);
 Route::get('sections/{id}', [SectionController::class, 'show']);
+Route::get('modules', [ModuleController::class, 'index']);
+Route::get('modules/{id}', [ModuleController::class, 'show']);
+Route::get('lessons', [LessonController::class, 'index']);
+Route::get('lessons/{id}', [LessonController::class, 'show']);
 
 Route::prefix('teachers')->middleware('role:teacher')->group(function () {
     Route::post('courses', [CourseController::class, 'store']);
@@ -21,5 +27,10 @@ Route::prefix('teachers')->middleware('role:teacher')->group(function () {
     Route::post('sections', [SectionController::class, 'store']);
     Route::put('sections/{id}', [SectionController::class, 'update']);
     Route::delete('sections/{id}', [SectionController::class, 'destroy']);
-
+    Route::post('modules', [ModuleController::class, 'store']);
+    Route::put('modules/{id}', [ModuleController::class, 'update']);
+    Route::delete('modules/{id}', [ModuleController::class, 'destroy']);
+    Route::post('lessons', [LessonController::class, 'store']);
+    Route::put('lessons/{id}', [LessonController::class, 'update']);
+    Route::delete('lessons/{id}', [LessonController::class, 'destroy']);
 });
