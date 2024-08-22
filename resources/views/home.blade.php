d90c396c58f9eaa82f079e101521f24f09ea595e
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-08-22 14:53:27 +0200

    authentication fully working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,6 @@
 @section('content')
     <div class="container">
         <h1>Welcome to My Application</h1>
-        <div class="buttons">
-            <a href="{{ url('/teachers/dashboard') }}" class="btn btn-primary">Teacher Access</a>
-            <a href="{{ url('/students/dashboard') }}" class="btn btn-secondary">Student Access</a>
-        </div>
+
     </div>
 @endsection
