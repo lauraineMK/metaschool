fbc367be5e645cb8e63266cd9df2fb0f94842f68
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-08-27 11:59:31 +0200

    solved seeders' error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'firstname' => 'Test User\'s firstname',
+            'surname' => 'Test User\'s surname',
             'email' => 'test@example.com',
         ]);
     }
