4e3f7642f34be26eb6722a0f42c2aec42e826e10
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-08-27 11:53:07 +0200

    added success and error messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,7 @@
     </header>
 
     <div class="container">
-        @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-        @endif
 
-        @if (session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-        @endif
         @yield('content')
     </div>
 
