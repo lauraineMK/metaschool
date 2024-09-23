a410e23efd32b212de6ac55e3939c3a1bddfea3c
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-09-23 16:50:09 +0200

    The 'lesson viewed' button can also be viewed both in the lesson index view and the course show one

--- resources/views/student/lessons/index.blade.php
+++ resources/views/student/lessons/index.blade.php
@@ -25,10 +25,12 @@
             @foreach ($lessons as $lesson)
             <tr>
                 <td>{{ $lesson->id }}</td>
-                <td class="d-flex justify-content-between align-items-center">{{ $lesson->title }}
-                <button id="lesson-viewed-btn" data-lesson-id="{{ $lesson->id }}" class="btn btn-outline-secondary">
+                <td>
+                    <div class="d-flex justify-content-between align-items-center">{{ $lesson->title }}
+                        <button id="lesson-viewed-btn" data-lesson-id="{{ $lesson->id }}" class="btn btn-outline-secondary">
                             <span class="fa fa-check-circle"></span>
                         </button>
+                    </div>
                 </td>
                 <td class="hide-on-small-mobile">{{ Str::limit($lesson->content, 50) }}</td>
                 <td>{{ $lesson->course->name }}</td>
